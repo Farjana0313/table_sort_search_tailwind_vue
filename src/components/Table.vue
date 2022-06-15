11fba7dde12e456ecb9c19c55040e9704aa4a33f
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2022-06-15 15:25:55 +0600

    fist commit

---
+++ src/components/Table.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="p-10">
+    <table>
+      <thead>
+        <tr>
+          <th class="border-2 p-2 text-left">Name</th>
+          <th class="border-2 p-2 text-left">Age</th>
+          <th class="border-2 p-2 text-left">Profession</th>
+          <th class="border-2 p-2 text-left">Year of birth</th>
+        </tr>
+      </thead>
+      <tbody>
+        <tr>
+          <td class="border-2 p-2">Manoj</td>
+          <td class="border-2 p-2">24</td>
+          <td class="border-2 p-2">Software Developer</td>
+          <td class="border-2 p-2">1997</td>
+        </tr>
+      </tbody>
+    </table>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Table',
+  data() {
+    return {
+      rows: [],
+      columns:[]
+    }
+  }
+}
+</script>
