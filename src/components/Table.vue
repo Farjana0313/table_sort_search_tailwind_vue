297ea5ddffa15412e5a3cc90fb7770386cb62b92
Farjana Akter <30821829+Farjana0313@users.noreply.github.com>
2022-06-15 18:00:41 +0600

    search 26m

--- src/components/Table.vue
+++ src/components/Table.vue
@@ -1,20 +1,17 @@
 <template>
   <div class="p-10">
+    <div>
+      <input type="text" class="border-solid border-2 mb-5 rounded h-5 w-64 p-2" placeholder="Search Record" @input="onSearch">
+    </div>
     <table>
       <thead>
         <tr>
-          <th class="border-2 p-2 text-left">Name</th>
-          <th class="border-2 p-2 text-left">Age</th>
-          <th class="border-2 p-2 text-left">Profession</th>
-          <th class="border-2 p-2 text-left">Year of birth</th>
+          <th v-for="(column, index) in columns" :key="index" class="border-solid border-2 p-2 text-left" @click="sortRecords(index)">{{ column }}</th>
         </tr>
       </thead>
       <tbody>
-        <tr>
-          <td class="border-2 p-2">Manoj</td>
-          <td class="border-2 p-2">24</td>
-          <td class="border-2 p-2">Software Developer</td>
-          <td class="border-2 p-2">1997</td>
+        <tr v-for="(row,index) in rows" :key="index">
+          <td v-for="(rowItem, itemIndex) in row" :key="itemIndex" class="border-solid border-2 p-2">{{ rowItem }}</td>
         </tr>
       </tbody>
     </table>
@@ -22,13 +19,62 @@
 </template>
 
 <script>
+
+const performSearch = (rows, term) => {
+  const results = rows.filter(
+    row=>row.join("").toLowerCase().includes(term.toLowerCase())
+  )
+  return results;
+}
+
 export default {
   name: 'Table',
   data() {
     return {
-      rows: [],
-      columns:[]
+      rawRows: [
+        [
+          "Manoj", "24", "Software Developer", "1997"
+        ],
+        [
+          "Jhoni", "26", "Lawyer", "1995"
+        ],
+        [
+          "Sams", "27", "Lawyer", "1995"
+        ],
+        [
+          "William", "28", "Lawyer", "1996"
+        ],
+        [
+          "Anna", "26", "Lawyer", "1997"
+        ]
+      ],
+      rows:[],
+      columns: [
+        'Name',
+        'Age',
+        'Profession',
+        'Year of birth'
+      ],
+      sortIndex: null,
+      sortDirection:null,
     }
+  },
+  methods: {
+    onSearch(e) {
+      const term = e.target.value;
+      this.rows = performSearch(this.rawRows, term);
+    },
+    sortRecords(index) {
+      this.sortIndex = index;
+      this.rows = this.rows.sort(
+        (rowA, rowB) => {
+          return rowA[index].localeCompare(rowB[index])
+        }
+      )
+    }
+  },
+  mounted() {
+    this.rows = this.rawRows;
   }
 }
 </script>
